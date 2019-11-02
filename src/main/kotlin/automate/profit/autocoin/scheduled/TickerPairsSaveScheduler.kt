b6b0516cdc566809e769578917f2b8ca18877fb7
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2019-11-02 22:51:43 +0100

    Change logging level of fetching tickers to WARN

--- src/main/kotlin/automate/profit/autocoin/scheduled/TickerPairsSaveScheduler.kt
+++ src/main/kotlin/automate/profit/autocoin/scheduled/TickerPairsSaveScheduler.kt
@@ -2,13 +2,16 @@ package automate.profit.autocoin.scheduled
 
 import automate.profit.autocoin.exchange.ticker.FileTickerPairRepository
 import automate.profit.autocoin.exchange.ticker.TickerPairCache
+import mu.KLogging
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
 class TickerPairsSaveScheduler(private val tickerPairCache: TickerPairCache, private val tickerPairRepository: FileTickerPairRepository) {
+    companion object: KLogging()
     private val executorService = Executors.newScheduledThreadPool(1)
 
     fun scheduleSavingTickerPairs() {
+        logger.info { "Will save ticker pairs every 30 seconds" }
         executorService.scheduleAtFixedRate({
             tickerPairCache.getCurrencyPairWithExchangePairs().forEach {
                 tickerPairRepository.saveAll(it, tickerPairCache.getTickerCurrencyPairs(it))
