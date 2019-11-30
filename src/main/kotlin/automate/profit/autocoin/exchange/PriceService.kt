7911b1eeffc73a885b715bd797104113c5aa514b
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2019-11-30 13:57:28 +0100

    Improve concurrency writes for price cache

--- src/main/kotlin/automate/profit/autocoin/exchange/PriceService.kt
+++ src/main/kotlin/automate/profit/autocoin/exchange/PriceService.kt
@@ -1,11 +1,15 @@
 package automate.profit.autocoin.exchange
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import mu.KLogging
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.math.BigDecimal
 import java.time.Duration
 import java.time.temporal.ChronoUnit
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.locks.ReadWriteLock
+import java.util.concurrent.locks.ReentrantReadWriteLock
 
 
 data class PriceDto(
@@ -25,40 +29,55 @@ class PriceService(private val priceApiUrl: String,
             val calculatedAtMillis: Long
     )
 
-    private val priceCache = mutableMapOf<String, ValueWithTimestamp>()
+    private val priceCache = ConcurrentHashMap<String, ValueWithTimestamp>()
 
-    @Synchronized
+    companion object : KLogging()
+
+    private val currencyCodeLocks = ConcurrentHashMap<String, ReadWriteLock>()
+
     fun getUsdValue(currencyCode: String, amount: BigDecimal): BigDecimal {
+        if (currencyCode == "USD") {
+            return amount
+        }
+        val lock = currencyCodeLocks.computeIfAbsent(currencyCode) {
+            ReentrantReadWriteLock()
+        }
+
+        lock.writeLock().lock()
         if (priceCache.containsKey(currencyCode)) {
             val valueWithTimestamp = priceCache[currencyCode]!!
             if (isOlderThanMaxCacheAge(valueWithTimestamp.calculatedAtMillis)) {
-                priceCache[currencyCode] = ValueWithTimestamp(
-                        calculatedAtMillis = currentTimeMillis(),
-                        value = fetchUsdPrice(currencyCode)
-                )
+                priceCache.remove(currencyCode)
             }
-        } else {
-            priceCache[currencyCode] = ValueWithTimestamp(
+        }
+
+        priceCache.computeIfAbsent(currencyCode) {
+            ValueWithTimestamp(
                     calculatedAtMillis = currentTimeMillis(),
                     value = fetchUsdPrice(currencyCode)
             )
         }
+        lock.writeLock().unlock()
+
         val price = priceCache.getValue(currencyCode).value
+
         return amount.multiply(price)
     }
+
 
     private fun isOlderThanMaxCacheAge(calculatedAtMillis: Long): Boolean {
         return currentTimeMillis() - calculatedAtMillis > maxPriceCacheAge
     }
 
     private fun fetchUsdPrice(currencyCode: String): BigDecimal {
+        logger.info { "Fetching price for $currencyCode" }
         val request = Request.Builder()
                 .url("$priceApiUrl/prices/USD?currencyCodes=${currencyCode}")
                 .get()
                 .build()
         val priceResponse = httpClient.newCall(request).execute()
         priceResponse.use {
-            check(priceResponse.code == 200) { "Could not get price for $currencyCode" }
+            check(priceResponse.code == 200) { "Could not get price for $currencyCode, code=${priceResponse.code}" }
             val priceDto = objectMapper.readValue(priceResponse.body?.string(), Array<PriceDto>::class.java)
             check(priceDto.size == 1) { "No required price in response for $currencyCode" }
             return priceDto.first().price.toBigDecimal()
