4a3a503e3917be07cd9e4a2263944e83bdd4986d
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2019-11-30 14:05:05 +0100

    Decrease concurrency writes for price cache because of too many open http connections

--- src/main/kotlin/automate/profit/autocoin/exchange/arbitrage/TwoLegArbitrageProfitCalculator.kt
+++ src/main/kotlin/automate/profit/autocoin/exchange/arbitrage/TwoLegArbitrageProfitCalculator.kt
@@ -3,6 +3,7 @@ package automate.profit.autocoin.exchange.arbitrage
 import automate.profit.autocoin.exchange.PriceService
 import automate.profit.autocoin.exchange.ticker.CurrencyPairWithExchangePair
 import automate.profit.autocoin.exchange.ticker.TickerPair
+import mu.KLogging
 import java.math.BigDecimal.ONE
 import java.math.BigDecimal.ZERO
 import java.math.RoundingMode.HALF_UP
@@ -11,45 +12,51 @@ class TwoLegArbitrageProfitCalculator(
         private val priceService: PriceService,
         private val currentTimeMillis: () -> Long = System::currentTimeMillis
 ) {
+    companion object : KLogging()
 
     fun calculateProfits(currencyPairWithExchangePair: CurrencyPairWithExchangePair, tickerPairs: List<TickerPair>): List<TwoLegArbitrageProfit> {
         return tickerPairs.mapNotNull { calculateProfit(currencyPairWithExchangePair, it) }
     }
 
     fun calculateProfit(currencyPairWithExchangePair: CurrencyPairWithExchangePair, tickerPair: TickerPair): TwoLegArbitrageProfit? {
-        val currentTimeMillis = currentTimeMillis()
-        return when {
-            tickerPair.first.bid > tickerPair.second.bid -> // sell on first, buy on second
-                TwoLegArbitrageProfit(
-                        currencyPair = currencyPairWithExchangePair.currencyPair,
-                        exchangePair = currencyPairWithExchangePair.exchangePair,
-                        sellAtExchange = currencyPairWithExchangePair.exchangePair.firstExchange,
-                        buyAtExchange = currencyPairWithExchangePair.exchangePair.secondExchange,
-                        buyPrice = tickerPair.second.bid,
-                        sellPrice = tickerPair.first.bid,
+        return try {
+            val currentTimeMillis = currentTimeMillis()
+            when {
+                tickerPair.first.bid > tickerPair.second.bid -> // sell on first, buy on second
+                    TwoLegArbitrageProfit(
+                            currencyPair = currencyPairWithExchangePair.currencyPair,
+                            exchangePair = currencyPairWithExchangePair.exchangePair,
+                            sellAtExchange = currencyPairWithExchangePair.exchangePair.firstExchange,
+                            buyAtExchange = currencyPairWithExchangePair.exchangePair.secondExchange,
+                            buyPrice = tickerPair.second.bid,
+                            sellPrice = tickerPair.first.bid,
 
-                        usd24hVolumeAtBuyExchange = priceService.getUsdValue(tickerPair.second.currencyPair.counter, tickerPair.second.counterCurrency24hVolume),
-                        usd24hVolumeAtSellExchange = priceService.getUsdValue(tickerPair.first.currencyPair.counter, tickerPair.first.counterCurrency24hVolume),
+                            usd24hVolumeAtBuyExchange = priceService.getUsdValue(tickerPair.second.currencyPair.counter, tickerPair.second.counterCurrency24hVolume),
+                            usd24hVolumeAtSellExchange = priceService.getUsdValue(tickerPair.first.currencyPair.counter, tickerPair.first.counterCurrency24hVolume),
 
-                        relativeProfit = if (tickerPair.second.bid > ZERO) tickerPair.first.bid.divide(tickerPair.second.bid, HALF_UP) - ONE else ZERO,
-                        calculatedAtMillis = currentTimeMillis
-                )
-            tickerPair.second.bid > tickerPair.first.bid -> // sell on second, buy on first
-                TwoLegArbitrageProfit(
-                        currencyPair = currencyPairWithExchangePair.currencyPair,
-                        exchangePair = currencyPairWithExchangePair.exchangePair,
-                        sellAtExchange = currencyPairWithExchangePair.exchangePair.secondExchange,
-                        buyAtExchange = currencyPairWithExchangePair.exchangePair.firstExchange,
-                        buyPrice = tickerPair.first.bid,
-                        sellPrice = tickerPair.second.bid,
+                            relativeProfit = if (tickerPair.second.bid > ZERO) tickerPair.first.bid.divide(tickerPair.second.bid, HALF_UP) - ONE else ZERO,
+                            calculatedAtMillis = currentTimeMillis
+                    )
+                tickerPair.second.bid > tickerPair.first.bid -> // sell on second, buy on first
+                    TwoLegArbitrageProfit(
+                            currencyPair = currencyPairWithExchangePair.currencyPair,
+                            exchangePair = currencyPairWithExchangePair.exchangePair,
+                            sellAtExchange = currencyPairWithExchangePair.exchangePair.secondExchange,
+                            buyAtExchange = currencyPairWithExchangePair.exchangePair.firstExchange,
+                            buyPrice = tickerPair.first.bid,
+                            sellPrice = tickerPair.second.bid,
 
-                        usd24hVolumeAtBuyExchange = priceService.getUsdValue(tickerPair.first.currencyPair.counter, tickerPair.first.counterCurrency24hVolume),
-                        usd24hVolumeAtSellExchange = priceService.getUsdValue(tickerPair.second.currencyPair.counter, tickerPair.second.counterCurrency24hVolume),
+                            usd24hVolumeAtBuyExchange = priceService.getUsdValue(tickerPair.first.currencyPair.counter, tickerPair.first.counterCurrency24hVolume),
+                            usd24hVolumeAtSellExchange = priceService.getUsdValue(tickerPair.second.currencyPair.counter, tickerPair.second.counterCurrency24hVolume),
 
-                        relativeProfit = if (tickerPair.first.bid > ZERO) tickerPair.second.bid.divide(tickerPair.first.bid, HALF_UP) - ONE else ZERO,
-                        calculatedAtMillis = currentTimeMillis
-                )
-            else -> null
+                            relativeProfit = if (tickerPair.first.bid > ZERO) tickerPair.second.bid.divide(tickerPair.first.bid, HALF_UP) - ONE else ZERO,
+                            calculatedAtMillis = currentTimeMillis
+                    )
+                else -> null
+            }
+        } catch (e: Exception) {
+            logger.error(e) { "Could not calculate two leg arbitrage profit" }
+            null
         }
     }
 
