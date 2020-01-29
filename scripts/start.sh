09b87a571c77300e49f5cbd8051717e3f0bb5698
JacquesBaniaque <jb@jb>
2020-01-29 22:48:44 +0100

    Limit visibility to localhost. It shouldn't be accessible directly by port number from the outside

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,70 +1,54 @@
 #!/usr/bin/env bash
 
 preconditions() {
-    if [[ -f "env.properties" ]]
-    then
-        . "env.properties"
-    else
-        echo "Can't find env.properties. Maybe forgot to create one in scripts dir?"
-        exit 100
-    fi
+  if [[ -f "env.properties" ]]; then
+    . "./env.properties"
+  else
+    echo "env.properties not found"
+    exit 9
+  fi
 
-    if [[ -z ${VERSION} ]]; then
-        echo "VERSION not set. Please run the script with VERSION variable set: VERSION=x.x.x sudo -E ./start.sh";
-        exit 101;
-    else
-        VERSION="autocoin-arbitrage-monitor-${VERSION}";
-    fi
+  declare -a requiredVariables=(
+    "APP_PORT_ON_HOST"
+    "LOG_PATH"
+    "APP_DATA_PATH"
+    "SERVICE_NAME"
+    "APP_OAUTH_CLIENT_ID"
+    "APP_OAUTH_CLIENT_SECRET"
+  )
 
-    if [[ -z ${APP_PORT_ON_HOST} ]]; then
-        echo "APP_PORT_ON_HOST not set. Please edit env.properties file in deployment directory.";
-        exit 104;
+  for requiredVariable in "${requiredVariables[@]}"; do
+    if [ -z "$requiredVariable" ]; then
+      echo "$requiredVariable not set. Please edit env.properties file in deployment directory."
+      exit 99
     fi
+  done
 
-    if [[ -z ${LOG_PATH} ]]; then
-        echo "LOG_PATH not set. Please edit env.properties file in deployment directory.";
-        exit 105;
-    fi
-
-    if [[ -z ${APP_DATA_PATH} ]]; then
-        echo "APP_DATA_PATH not set. Please edit env.properties file in deployment directory.";
-        exit 106;
-    fi
-
-    if [[ -z ${SERVICE_NAME} ]]; then
-        echo "SERVICE_NAME not set. Please edit env.properties file in deployment directory.";
-        exit 107;
-    fi
-
-    if [[ -z ${APP_OAUTH_CLIENT_ID} ]]; then
-        echo "APP_OAUTH_CLIENT_ID not set. Please edit env.properties file in deployment directory.";
-        exit 107;
-    fi
-
-    if [[ -z ${APP_OAUTH_CLIENT_SECRET} ]]; then
-        echo "APP_OAUTH_CLIENT_SECRET not set. Please edit env.properties file in deployment directory.";
-        exit 107;
-    fi
+  if [[ -z "$VERSION" ]]; then
+    VERSION="latest"
+  else
+    VERSION="${SERVICE_NAME}-${VERSION}"
+  fi
 }
 
 preconditions
 
 # Run new container
-echo "Starting new version of container. Using version: ${VERSION}";
-echo "Using port: ${APP_PORT_ON_HOST}";
+echo "Starting new version of container. Using version: ${VERSION}"
+echo "Using port: ${APP_PORT_ON_HOST}"
 
 # Use JAVA_OPTS="-XX:+ExitOnOutOfMemoryError" to prevent from running when any of threads runs of out memory and dies
 
 docker run --name ${SERVICE_NAME} -d \
-    -p ${APP_PORT_ON_HOST}:10021 \
-    -e BASIC_PASS=${BASIC_PASS} \
-    -e DOCKER_TAG=${VERSION} \
-    -e JAVA_OPTS="-XX:+ExitOnOutOfMemoryError" \
-    -e APP_OAUTH_CLIENT_ID=${APP_OAUTH_CLIENT_ID} \
-    -e APP_OAUTH_CLIENT_SECRET=${APP_OAUTH_CLIENT_SECRET} \
-    -v ${LOG_PATH}:/app/log \
-    -v ${APP_DATA_PATH}:/app/data \
-    --memory=1200m \
-    --restart=no \
-    --network autocoin-services-admin \
-    localhost:5000/autocoin-arbitrage-monitor:${VERSION}
+  -p 127.0.0.1:${APP_PORT_ON_HOST}:10021 \
+  -e BASIC_PASS=${BASIC_PASS} \
+  -e DOCKER_TAG=${VERSION} \
+  -e JAVA_OPTS="-XX:+ExitOnOutOfMemoryError" \
+  -e APP_OAUTH_CLIENT_ID=${APP_OAUTH_CLIENT_ID} \
+  -e APP_OAUTH_CLIENT_SECRET=${APP_OAUTH_CLIENT_SECRET} \
+  -v ${LOG_PATH}:/app/log \
+  -v ${APP_DATA_PATH}:/app/data \
+  --memory=1200m \
+  --restart=no \
+  --network autocoin-services-admin \
+  localhost:5000/autocoin-arbitrage-monitor:${VERSION}
