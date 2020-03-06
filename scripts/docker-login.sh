7eb25ee0acde084ff3f4aadd20e91bd720d27c09
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2020-03-06 17:42:26 +0100

    Move -XX:+ExitOnOutOfMemoryError to Dockerfile

--- scripts/docker-login.sh
+++ scripts/docker-login.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
 
-docker exec -it autocoin-arbitrage-monitor /bin/sh
+preconditions() {
+    if [[ -f "env.properties" ]]
+    then
+        . "env.properties"
+    else
+        echo "Can't find env.properties. Maybe forgot to create one in scripts dir?"
+        exit 100
+    fi
+
+  declare -a requiredVariables=(
+    "SERVICE_NAME"
+  )
+
+  for requiredVariable in "${requiredVariables[@]}"; do
+    if [ -z "${!requiredVariable}" ]; then
+      echo "$requiredVariable not set. Please edit env.properties file in deployment directory."
+      exit 1
+    fi
+  done
+}
+
+preconditions
+
+docker exec -it ${SERVICE_NAME} /bin/sh
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
+# turn on debug mode
+set -x
 
 preconditions() {
-    if [[ -z ${LOGS_PATH} ]]; then
-        echo "LOGS_PATH not set. Please edit env.properties file in deployment directory.";
-        exit 105;
+    if [[ -f "env.properties.template" ]]
+    then
+        . "env.properties.template"
+    else
+        echo "Can't find env.properties.template. Maybe forgot to create one in scripts dir?"
+        exit 100
     fi
 
-    if [[ -z ${DATA_PATH} ]]; then
-        echo "DATA_PATH not set. Please edit env.properties file in deployment directory.";
-        exit 106;
+  declare -a requiredVariables=(
+    "LOGS_PATH"
+    "DATA_PATH"
+    "APPS_PATH"
+    "SERVICE_NAME"
+    "LOG_PATH"
+  )
+
+  for requiredVariable in "${requiredVariables[@]}"; do
+    if [ -z "${!requiredVariable}" ]; then
+      echo "$requiredVariable not set. Please edit env.properties file in deployment directory or provide variable in the environment."
+      exit 1
     fi
+  done
 
 }
 
@@ -17,11 +32,10 @@ preconditions
 
 # Installs scripts to app working directory on server
 # IMPORTANT! Run with: sudo -E ./install.sh
-SERVICE_NAME="autocoin-arbitrage-monitor"
 TARGET_DIR="${APPS_PATH}/${SERVICE_NAME}"
 mkdir -p ${TARGET_DIR}
-mkdir -p ${LOGS_PATH}/${SERVICE_NAME}
-ln -s ${LOGS_PATH}/${SERVICE_NAME} ${TARGET_DIR}/log
+mkdir -p ${LOG_PATH}
+ln -s ${LOG_PATH} ${TARGET_DIR}/log
 mkdir -p ${DATA_PATH}/${SERVICE_NAME}
 ln -s ${DATA_PATH}/${SERVICE_NAME} ${TARGET_DIR}/data
 cp start.sh ${TARGET_DIR}
