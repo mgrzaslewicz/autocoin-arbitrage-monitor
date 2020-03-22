d433885966de50088809d70383f7eeba97f2bd18
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2020-03-22 17:57:55 +0100

    Add requesting order book SSE

--- src/test/kotlin/automate/profit/autocoin/RunLocal.kt
+++ src/test/kotlin/automate/profit/autocoin/RunLocal.kt
@@ -17,7 +17,6 @@ fun main() {
     setProperty("SERVICE_NAME", "autocoin-arbitrage-service")
     setProperty("APP_OAUTH_CLIENT_ID", "changeme")
     setProperty("APP_OAUTH_CLIENT_SECRET", "changeme")
-    setProperty("APP_SAVE_METRICS_TO_FILE_EVERY_N_SECONDS", "60")
 //    setProperty("APP_USE_HARDCODED_TWO_LEG_ARBITRAGE_CURRENCY_AND_EXCHANGE_PAIRS", "true")
     setProperty("APP_EXCHANGES_TO_MONITOR_TWO_LEG_ARBITRAGE_OPPORTUNITIES", "bibex,binance,bittrex")
 //    setProperty("APP_TWO_LEG_ARBITRAGE_CURRENCY_PAIRS_WHITE_LIST", "QTUM/BTC,GAS/BTC")
