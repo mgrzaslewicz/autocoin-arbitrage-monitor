9258d0c1606e6ffbb493052f60d89083cd0aecb8
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2022-08-01 14:13:32 +0200

    Provide default oauth2 client secret for local development

--- src/test/kotlin/automate/profit/autocoin/RunLocal.kt
+++ src/test/kotlin/automate/profit/autocoin/RunLocal.kt
@@ -12,9 +12,8 @@ import java.lang.System.setProperty
 fun main() {
     setProperty("OAUTH2_API_URL", "http://localhost:9002")
     setProperty("EXCHANGE_MEDIATOR_API_URL", "http://localhost:9001")
-    setProperty("SERVICE_NAME", "autocoin-arbitrage-service")
-    setProperty("APP_OAUTH_CLIENT_ID", "changeme")
-    setProperty("APP_OAUTH_CLIENT_SECRET", "changeme")
+    setProperty("SERVICE_NAME", "autocoin-arbitrage-monitor")
+    setProperty("APP_OAUTH_CLIENT_SECRET", "arbitrage-monitor-secret-for-local-development")
 //    setProperty("APP_USE_HARDCODED_TWO_LEG_ARBITRAGE_CURRENCY_AND_EXCHANGE_PAIRS", "true")
     setProperty("APP_EXCHANGES_TO_MONITOR_TWO_LEG_ARBITRAGE_OPPORTUNITIES", "bibex,binance,bittrex")
 //    setProperty("APP_TWO_LEG_ARBITRAGE_CURRENCY_PAIRS_WHITE_LIST", "QTUM/BTC,GAS/BTC")
