397a4af4fcbd8b84b789e3021ccfab8579e18b0f
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-07-10 19:23:02 +0200

    Keep config loading as separate class

--- src/main/kotlin/automate/profit/autocoin/app/config/AppConfig.kt
+++ src/main/kotlin/automate/profit/autocoin/app/config/AppConfig.kt
@@ -2,10 +2,7 @@ package automate.profit.autocoin.app.config
 
 import automate.profit.autocoin.exchange.SupportedExchange
 import automate.profit.autocoin.exchange.currency.CurrencyPair
-import com.typesafe.config.ConfigFactory
 import java.io.File
-import java.lang.System.getProperty
-import java.lang.System.getenv
 import java.math.BigDecimal
 import java.time.Duration
 
@@ -39,48 +36,4 @@ data class AppConfig(
     // endregion
 ) {
     val metricsFolder: String = appDataPath + File.separator + "metrics"
-}
-
-fun loadConfig(): AppConfig {
-    val defaultConfig = ConfigFactory.load("config/app-base")
-    val profile = getPropertyThenEnv("PROFILE")
-    val profileConfig = if (profile == null) {
-        ConfigFactory.empty()
-    } else {
-        ConfigFactory.parseResources("config/app-$profile.conf")
-    }
-    val config = ConfigFactory
-        .systemEnvironment()
-        .withFallback(ConfigFactory.systemProperties())
-        .withFallback(profileConfig)
-        .withFallback(defaultConfig)
-        .resolve()
-
-    return AppConfig(
-        serverPort = config.getInt("server.port"),
-        serviceName = config.getString("service.name"),
-
-        oauth2ApiUrl = config.getString("externalServices.oauth.apiUrl"),
-        oauth2ClientId = config.getString("externalServices.oauth.clientId"),
-        oauth2ClientSecret = config.getString("externalServices.oauth.clientSecret"),
-        exchangeMediatorApiUrl = config.getString("externalServices.exchangeMediator.apiUrl"),
-        telegrafHostname = config.getString("externalServices.telegrafHostname"),
-
-        appDataPath = config.getString("service.dataFolder"),
-
-        metricsDestination = MetricsDestination.valueOf(config.getString("metrics.destination")),
-
-        currencyPairsOverride = config.getStringList("arbitrage.currencyPairsOverride")
-            .map { CurrencyPair.of(it) }
-            .toSet(),
-        exchangesToMonitorOverride = config.getStringList("arbitrage.exchangesToMonitorOverride")
-            .map { SupportedExchange.fromExchangeName(it) },
-        twoLegArbitrageProfitCacheDuration = config.getDuration("arbitrage.ageOfOldestTwoLegArbitrageProfitToKeepInCache"),
-        orderBookUsdAmountThresholds = config.getIntList("arbitrage.orderBookUsdAmountThresholds")
-            .map { BigDecimal(it) },
-    )
-}
-
-private fun getPropertyThenEnv(propertyName: String): String? {
-    return getProperty(propertyName, getenv(propertyName))
 }
---
+++ src/main/kotlin/automate/profit/autocoin/app/config/ConfigLoader.kt
@@ -0,0 +1,49 @@
+package automate.profit.autocoin.app.config
+
+import automate.profit.autocoin.exchange.SupportedExchange
+import automate.profit.autocoin.exchange.currency.CurrencyPair
+import com.typesafe.config.ConfigFactory
+import java.math.BigDecimal
+
+class ConfigLoader {
+    companion object {
+        fun loadConfig(profile: String? = System.getProperty("PROFILE", System.getenv("PROFILE"))): AppConfig {
+            val defaultConfig = ConfigFactory.load("config/app-base")
+            val profileConfig = if (profile == null) {
+                ConfigFactory.empty()
+            } else {
+                ConfigFactory.parseResources("config/app-$profile.conf")
+            }
+            val config = ConfigFactory
+                .systemEnvironment()
+                .withFallback(ConfigFactory.systemProperties())
+                .withFallback(profileConfig)
+                .withFallback(defaultConfig)
+                .resolve()
+
+            return AppConfig(
+                serverPort = config.getInt("server.port"),
+                serviceName = config.getString("service.name"),
+
+                oauth2ApiUrl = config.getString("externalServices.oauth.apiUrl"),
+                oauth2ClientId = config.getString("externalServices.oauth.clientId"),
+                oauth2ClientSecret = config.getString("externalServices.oauth.clientSecret"),
+                exchangeMediatorApiUrl = config.getString("externalServices.exchangeMediator.apiUrl"),
+                telegrafHostname = config.getString("externalServices.telegrafHostname"),
+
+                appDataPath = config.getString("service.dataFolder"),
+
+                metricsDestination = MetricsDestination.valueOf(config.getString("metrics.destination")),
+
+                currencyPairsOverride = config.getStringList("arbitrage.currencyPairsOverride")
+                    .map { CurrencyPair.of(it) }
+                    .toSet(),
+                exchangesToMonitorOverride = config.getStringList("arbitrage.exchangesToMonitorOverride")
+                    .map { SupportedExchange.fromExchangeName(it) },
+                twoLegArbitrageProfitCacheDuration = config.getDuration("arbitrage.ageOfOldestTwoLegArbitrageProfitToKeepInCache"),
+                orderBookUsdAmountThresholds = config.getIntList("arbitrage.orderBookUsdAmountThresholds")
+                    .map { BigDecimal(it) },
+            )
+        }
+    }
+}
