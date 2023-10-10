86807213394107dc31e7b08f3a50d8a6026beafa
Mikolaj Grzaslewicz <mgrzaslewicz@atlassian.com>
2023-10-10 22:10:28 +0200

    Add support for multiple config profiles

--- src/main/kotlin/automate/profit/autocoin/Application.kt
+++ src/main/kotlin/automate/profit/autocoin/Application.kt
@@ -3,7 +3,7 @@ package automate.profit.autocoin
 import automate.profit.autocoin.app.AppContext
 import automate.profit.autocoin.app.AppStarter
 import automate.profit.autocoin.app.AppVersion
-import automate.profit.autocoin.app.config.ConfigLoader.Companion.loadConfig
+import automate.profit.autocoin.app.config.ConfigLoader.loadConfig
 import mu.KotlinLogging
 import java.net.SocketAddress
 import kotlin.system.measureTimeMillis
--- src/main/kotlin/automate/profit/autocoin/app/config/ConfigLoader.kt
+++ src/main/kotlin/automate/profit/autocoin/app/config/ConfigLoader.kt
@@ -2,48 +2,54 @@ package automate.profit.autocoin.app.config
 
 import automate.profit.autocoin.exchange.SupportedExchange
 import automate.profit.autocoin.exchange.currency.CurrencyPair
+import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
+import mu.KotlinLogging
 import java.math.BigDecimal
 
-class ConfigLoader {
-    companion object {
-        fun loadConfig(profile: String? = System.getProperty("PROFILE", System.getenv("PROFILE"))): AppConfig {
-            val defaultConfig = ConfigFactory.load("config/app-base")
-            val profileConfig = if (profile == null) {
-                ConfigFactory.empty()
-            } else {
-                ConfigFactory.parseResources("config/app-$profile.conf")
+object ConfigLoader {
+    private val logger = KotlinLogging.logger {}
+    private fun getProfileConfig(profile: String): Config = ConfigFactory.parseResources("config/app-$profile.conf")
+    fun loadConfig(profiles: String? = System.getProperty("PROFILES", System.getenv("PROFILES"))): AppConfig {
+        val profiles = profiles?.split(",")
+        logger.info { "Loading config for profiles: $profiles" }
+
+        val defaultConfig = ConfigFactory.load("config/app-base")
+        val config = ConfigFactory
+            .systemEnvironment()
+            .withFallback(ConfigFactory.systemProperties())
+            .let {
+                var currentConfig = it
+                profiles?.forEach { profile ->
+                    currentConfig = currentConfig.withFallback(getProfileConfig(profile))
+                }
+                currentConfig
             }
-            val config = ConfigFactory
-                .systemEnvironment()
-                .withFallback(ConfigFactory.systemProperties())
-                .withFallback(profileConfig)
-                .withFallback(defaultConfig)
-                .resolve()
+            .withFallback(defaultConfig)
+            .resolve()
 
-            return AppConfig(
-                serverPort = config.getInt("server.port"),
-                serviceName = config.getString("service.name"),
+        return AppConfig(
+            serverPort = config.getInt("server.port"),
+            serviceName = config.getString("service.name"),
 
-                oauth2ApiUrl = config.getString("externalServices.oauth.apiUrl"),
-                oauth2ClientId = config.getString("externalServices.oauth.clientId"),
-                oauth2ClientSecret = config.getString("externalServices.oauth.clientSecret"),
-                exchangeMediatorApiUrl = config.getString("externalServices.exchangeMediator.apiUrl"),
-                telegrafHostname = config.getString("externalServices.telegrafHostname"),
+            oauth2ApiUrl = config.getString("externalServices.oauth.apiUrl"),
+            oauth2ClientId = config.getString("externalServices.oauth.clientId"),
+            oauth2ClientSecret = config.getString("externalServices.oauth.clientSecret"),
+            exchangeMediatorApiUrl = config.getString("externalServices.exchangeMediator.apiUrl"),
+            telegrafHostname = config.getString("externalServices.telegrafHostname"),
 
-                appDataPath = config.getString("service.dataFolder"),
+            appDataPath = config.getString("service.dataFolder"),
 
-                metricsDestination = MetricsDestination.valueOf(config.getString("metrics.destination")),
+            metricsDestination = MetricsDestination.valueOf(config.getString("metrics.destination")),
 
-                currencyPairsOverride = config.getStringList("arbitrage.currencyPairsOverride")
-                    .map { CurrencyPair.of(it) }
-                    .toSet(),
-                exchangesToMonitorOverride = config.getStringList("arbitrage.exchangesToMonitorOverride")
-                    .map { SupportedExchange.fromExchangeName(it) },
-                twoLegArbitrageProfitCacheDuration = config.getDuration("arbitrage.twoLegArbitrageProfitCacheDuration"),
-                orderBookUsdAmountThresholds = config.getIntList("arbitrage.orderBookUsdAmountThresholds")
-                    .map { BigDecimal(it) },
-            )
-        }
+            currencyPairsOverride = config.getStringList("arbitrage.currencyPairsOverride")
+                .map { CurrencyPair.of(it) }
+                .toSet(),
+            exchangesToMonitorOverride = config.getStringList("arbitrage.exchangesToMonitorOverride")
+                .map { SupportedExchange.fromExchangeName(it) },
+            twoLegArbitrageProfitCacheDuration = config.getDuration("arbitrage.twoLegArbitrageProfitCacheDuration"),
+            orderBookUsdAmountThresholds = config.getIntList("arbitrage.orderBookUsdAmountThresholds")
+                .map { BigDecimal(it) },
+        )
     }
 }
